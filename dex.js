e310eb491a7f555688475dd813ed3bd99b90cc50
GabrielAruanda <garuanda@gmail.com>
2024-01-09 14:18:33 -0800

    first commit

---
+++ dex.js
@@ -0,0 +1,55 @@
+// let compras = ["abacaxi", "banana", "melão", "goiaba", "tangerina"]
+// alert (compras[0])
+// alert (compras[1])
+// alert (compras[2])
+// alert (compras[3])
+// alert (compras[4])
+// compras[0] = 'berga'
+// alert (compras[0])
+// compras [5] = 'doc'
+// alert (compras[5])
+
+// let dx = ["jm", "jn","jk","jg"];
+// dx.pop ();
+// alert (dx[0])
+// alert (dx[1])
+// alert (dx[2])
+// alert (dx[3])
+// dx.push("jj")
+// alert (dx[4])
+
+// let gbr = ["abd" , "cv" ,"vgg", "dgp"];
+// gbr.shift ("")
+// alert (gbr [0])
+// alert (gbr [1])
+// alert (gbr [2])
+// alert (gbr [3])
+// gbr.unshift ("cmv")
+// alert (gbr [0])
+
+/////  compras =>   0          1        2         3          4
+// let compras = ["abacaxi", "banana", "melão", "goiaba", "tangerina"]
+// let tamanho = compras.length  //5
+// alert (tamanho)
+// let i
+// for(i=0; i< tamanho; i=i+1)
+// {
+//     alert (compras[i])
+// }
+//  i=0 -> compras[0] -> abacaxi
+// i=0+1=1
+// i=1 -> compras[1] -> banana
+// i=1+1=2
+// i=2 -> compras[2] -> melão
+//i=2+1=3
+//i=3 -> compras[3] -> goiaba
+//i=3+1=4
+//i=4 -> compras[4] -> tangerina
+
+let marcas = ["Apple", "Dell", "Adidas", "Consul",]
+let n
+    for(n=0;n<jogo.length;n++){
+        alert(marcas[n])
+        alert(n)
+            document.getElementById(`item${n}`).innerText = marcas[n]
+    }
